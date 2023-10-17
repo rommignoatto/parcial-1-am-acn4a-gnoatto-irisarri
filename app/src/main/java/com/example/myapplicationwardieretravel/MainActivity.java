7050990458b9543a71a50b89bdfdb9becf87d9d3
rommignoatto <rommignoatto@gmail.com>
2023-10-17 16:38:27 -0300

    add event in buyButton

--- app/src/main/java/com/example/myapplicationwardieretravel/MainActivity.java
+++ app/src/main/java/com/example/myapplicationwardieretravel/MainActivity.java
@@ -3,12 +3,23 @@ package com.example.myapplicationwardieretravel;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button buyButtonLondon;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buyButtonLondon = findViewById(R.id.buyButton);
+
+    }
+
+    public void buyLondon(View v){
+        Toast.makeText(this, "Compraste paquete Londres histórica", Toast.LENGTH_SHORT).show();
     }
 }
