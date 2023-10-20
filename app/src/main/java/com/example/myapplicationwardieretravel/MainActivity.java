6c01946f1a97b27725ae89a37be6698d33853f13
rommignoatto <rommignoatto@gmail.com>
2023-10-20 20:19:41 -0300

    add header and logo at application start

--- app/src/main/java/com/example/myapplicationwardieretravel/MainActivity.java
+++ app/src/main/java/com/example/myapplicationwardieretravel/MainActivity.java
@@ -3,13 +3,20 @@ package com.example.myapplicationwardieretravel;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ScrollView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     Button buyButtonLondon;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,6 +24,24 @@ public class MainActivity extends AppCompatActivity {
 
         buyButtonLondon = findViewById(R.id.buyButton);
 
+
+
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.header_menu, menu);
+
+        MenuItem profileItem = menu.findItem(R.id.marca);
+        View actionView = profileItem.getActionView();
+
+        if (actionView != null) {
+            ImageView profileIcon = actionView.findViewById(R.id.marca);
+
+            TextView profileText = actionView.findViewById(R.id.perfil);
+        }
+
+        return true;
     }
 
     public void buyLondon(View v){
