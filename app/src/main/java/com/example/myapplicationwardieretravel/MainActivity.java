7c8c25a6c1a26293088bcc78471d25e0ee74d557
Natalia <Natalia@DESKTOP-MLJL964>
2023-11-30 22:15:42 -0300

    feat:add firebase setup

--- app/src/main/java/com/example/myapplicationwardieretravel/MainActivity.java
+++ app/src/main/java/com/example/myapplicationwardieretravel/MainActivity.java
@@ -43,6 +43,7 @@ public class MainActivity extends AppCompatActivity {
         buyButtonParis = findViewById(R.id.buyButtonParis);
 
 
+
     }
     public void openActivityHotel(View view){
         Intent intent = new Intent(this, MainActivityHoteles.class);
