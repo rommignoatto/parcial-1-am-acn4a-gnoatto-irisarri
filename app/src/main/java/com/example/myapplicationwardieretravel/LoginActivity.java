bce8ae1f42c5ddf85f7242baba08b3bdeb053d9d
Natalia <Natalia@DESKTOP-MLJL964>
2023-12-01 22:53:50 -0300

    feat: add mail password en firebase

--- app/src/main/java/com/example/myapplicationwardieretravel/LoginActivity.java
+++ app/src/main/java/com/example/myapplicationwardieretravel/LoginActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
     public void login (String email, String password){
@@ -13,12 +14,17 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     public void onLogButtonClick (View view){
+        EditText emailInput = findViewById(R.id.email);
+        EditText passInput = findViewById(R.id.password);
+
+        String email = emailInput.getText().toString();
+        String password = passInput.getText().toString();
         this.login("hola@romina.io", "prueba123");
 
     }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login2);
+        setContentView(R.layout.activity_login);
     }
 }
