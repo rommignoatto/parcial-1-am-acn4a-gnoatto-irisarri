957a20ecb945e7aa47e21005bdd8a576107905bb
Natalia <Natalia@DESKTOP-MLJL964>
2023-12-01 21:20:15 -0300

    feat: add butoon Login

--- app/src/main/java/com/example/myapplicationwardieretravel/LoginActivity.java
+++ app/src/main/java/com/example/myapplicationwardieretravel/LoginActivity.java
@@ -2,12 +2,26 @@ package com.example.myapplicationwardieretravel;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class LoginActivity extends AppCompatActivity {
+
+    public void login (String email, String password){
+        String tag;
+        Log.i( "firebase",  "email:" + email);
+        Log.i("firebase",  "password:" + password);
+    }
+
+    public void onLogButtonClick (View view){
+        this.login("hola@romina.io", "prueba123");
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login2);
+
     }
 }
