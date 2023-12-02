9fdfbf65ede25aa826ebbecfc2662a6950a1b1a6
Natalia <Natalia@DESKTOP-MLJL964>
2023-12-01 23:50:42 -0300

    feat: login with email and password

--- app/src/main/java/com/example/myapplicationwardieretravel/LoginActivity.java
+++ app/src/main/java/com/example/myapplicationwardieretravel/LoginActivity.java
@@ -1,16 +1,45 @@
 package com.example.myapplicationwardieretravel;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
-public class LoginActivity extends AppCompatActivity {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthActionCodeException;
+
+public class LoginActivity<EmailPasswordActivity> extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
     public void login (String email, String password){
         String tag;
         Log.i( "firebase",  "email:" + email);
         Log.i("firebase",  "password:" + password);
+
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()) {
+                            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                            startActivity(intent);
+
+                        }else {
+                            Toast.makeText(LoginActivity.this, "Fallo el login.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+
+
+                    }
+                });
     }
 
     public void onLogButtonClick (View view){
@@ -26,5 +55,6 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        mAuth = FirebaseAuth.getInstance();
     }
 }
